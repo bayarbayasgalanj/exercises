9a28a6e608680406cf984eba3498524292311e1b
bayarbayasgalanj <bayarbayasgalan.j@gmail.com>
2024-01-29 21:03:03 +0000

    5: Using the @PathVariable

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
 	@Autowired
@@ -29,6 +28,14 @@ class CashCardApplicationTests {
 
 		Double amount = documentContext.read("$.amount");
 		assertThat(amount).isEqualTo(123.45);
-		
+
+	}
+
+	@Test
+	void shouldNotReturnACashCardWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
 	}
 }
