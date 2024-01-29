ae5f4d0c036f4ba2d7734a25c74ae092131e9aa6
bayarbayasgalanj <bayarbayasgalan.j@gmail.com>
2024-01-29 23:47:59 +0000

    done Lab: Repositories & Spring Data

--- src/main/java/example/cashcard/CashCard.java
+++ src/main/java/example/cashcard/CashCard.java
@@ -1,6 +1,7 @@
 package example.cashcard;
 
+// Add this import
 import org.springframework.data.annotation.Id;
 
-record CashCard(Long id, Double amount) {
+record CashCard(@Id Long id, Double amount) {
 }
